dc7ed4eb3a61c1356ce9db8d02248e2708a1813f
Joe Boyce <boyce911@gmail.com>
2025-04-17 15:23:18 +0800

    Fix: add valid Navbar component

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,1 +1,11 @@
-// Sample content for Navbar.js
+import Link from 'next/link';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <Link href="/">🏠 Home</Link>
+      <Link href="/login">🔐 Login</Link>
+      <Link href="/watchlist">⭐ Watchlist</Link>
+    </nav>
+  );
+}
