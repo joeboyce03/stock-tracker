f4192a2ae7ae9c9b85fb0e48cc60a4d2f2f29afa
Joe Boyce <boyce911@gmail.com>
2025-04-17 15:17:05 +0800

    Add global navbar component and style

---
+++ components/Navbar.js
@@ -0,0 +1,1 @@
+// Sample content for Navbar.js
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '../styles/globals.css';
+import '../styles/navbar.css';
+import Navbar from '../components/Navbar';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
